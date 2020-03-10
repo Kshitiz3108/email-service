f80603b96477fb7bd78544064b31a418750cc32f
swap_96 <swap_96@SWAPNIL>
2020-03-10 19:40:23 +0530

    vhj

--- student/src/main/java/com/student/StudentApplication.java
+++ student/src/main/java/com/student/StudentApplication.java
@@ -1,14 +1,16 @@
-package com.student.app;
+package com.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
 @EntityScan(basePackages = {"com.student.model"})
-public class StudentApplication {
+public class StudentApplication extends SpringBootServletInitializer {
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
--- student/src/main/java/com/student/model/User.java
+++ student/src/main/java/com/student/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @Entity
-@Table(name="User")
+@Table(name="Users")
 @EnableAutoConfiguration
 public class User {
 	
